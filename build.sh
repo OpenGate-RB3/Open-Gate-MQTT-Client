e18b880be9da41ab2c6b60df2661c4e62eebabaf
Andrew Pegg <apegg@ucsd.edu>
2025-05-21 22:02:13 -0700

    simplified build script, on user to make sure arm compile is set up

--- build.sh
+++ build.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-set -e # crash and stop
-
-if [[ -z "$1" ]]; then
-    echo "must set path to sdk root: ${1}";
-    exit 1
-fi
-
-source $1/environment-setup-armv8-2a-qcom-linux
-
 mkdir build
 
 cd build
